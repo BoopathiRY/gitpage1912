a06e37a1dadb0e765b1e87a99fbe355dcf9bbb32
BoopathiRY <boopathi.r007@gmail.com>
2023-12-19 09:38:03 +0530

    task

---
+++ GiittPage.java
@@ -0,0 +1,21 @@
+package com.log;
+
+public class GiittPage {
+	
+	private void empid() {
+		System.out.println("employee id is: 1234");
+	}
+	
+	private void empname() {
+		System.out.println("employee name is: Albert");
+	}
+	public static void main(String[] args) {
+		
+		GiittPage g = new GiittPage();
+		
+		g.empid();
+		g.empname();
+	}
+	
+
+}
