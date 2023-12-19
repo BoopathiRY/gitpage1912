1372001c4da10ef1a51698d98b632719bb2e029f
BoopathiRY <boopathi.r007@gmail.com>
2023-12-19 10:52:05 +0530

    bike

---
+++ Bike.java
@@ -0,0 +1,20 @@
+package com.log;
+
+public class Bike {
+	
+	private void bikeBrand() {
+System.out.println("Royal Enfield");
+	}
+	
+	private void bikeModel() {
+System.out.println("Hunter 350");
+	}
+
+	public static void main(String[] args) {
+		Bike b = new Bike();
+		b.bikeBrand();
+		b.bikeModel();
+	}
+	
+
+}
