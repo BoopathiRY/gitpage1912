049d3096d78eef09bf86f625d58f21f3fb2f2d3d
BoopathiRY <boopathi.r007@gmail.com>
2023-12-19 10:55:17 +0530

    bike

--- Bike.java
+++ Bike.java
@@ -9,11 +9,14 @@ System.out.println("Royal Enfield");
 	private void bikeModel() {
 System.out.println("Hunter 350");
 	}
-
+	private void bikePrice() {
+System.out.println("1,75,000.Rs");
+	}
 	public static void main(String[] args) {
 		Bike b = new Bike();
 		b.bikeBrand();
 		b.bikeModel();
+		b.bikePrice();
 	}
 	
 
