b6994ab3a600a6eedbeb1b5ed894e277c23bbd51
BoopathiRY <boopathi.r007@gmail.com>
2023-12-19 11:01:18 +0530

    bike

--- Bike.java
+++ Bike.java
@@ -12,11 +12,15 @@ System.out.println("Hunter 350");
 	private void bikePrice() {
 System.out.println("1,75,000.Rs");
 	}
+	private void bikeMilage() {
+System.out.println("40kmpl");
+	}
 	public static void main(String[] args) {
 		Bike b = new Bike();
 		b.bikeBrand();
 		b.bikeModel();
 		b.bikePrice();
+		b.bikeMilage();
 	}
 	
 
