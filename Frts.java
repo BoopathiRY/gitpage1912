35e303cd4cfaa62676395c3be74b34a57994a5f7
BoopathiRY <boopathi.r007@gmail.com>
2023-12-20 06:11:42 +0530

    test1

--- Frts.java
+++ Frts.java
@@ -2,9 +2,7 @@ package com.log;
 
 public class Frts {
 
-	private void mango() {
-System.out.println("It tastes sour and sweet");
-	}
+	
 	private void apple() {
 System.out.println("It tastes sweet ");
 	}
@@ -15,7 +13,6 @@ public static void main(String[] args) {
 	
 	Frts f = new Frts();
 	
-	f.mango();
 	f.apple();
 	f.grape();
 	}	
