c34c1fb789091a484d1268309adfde41c79efd20
BoopathiRY <boopathi.r007@gmail.com>
2023-12-20 06:05:35 +0530

    day1

--- Frts.java
+++ Frts.java
@@ -8,15 +8,15 @@ System.out.println("It tastes sour and sweet");
 	private void apple() {
 System.out.println("It tastes sweet ");
 	}
+	private void grape() {
+System.out.println("It tastes sweet and sour");
+	}
 public static void main(String[] args) {
 	
 	Frts f = new Frts();
 	
 	f.mango();
 	f.apple();
+	f.grape();
+	}	
 }
-	
-	
-	
-}
-
